4ab5c1405c0bc8fe6ce819c94e3a99f6681b6974
Luis Felipe <llipeandrade34@gmail.com>
2022-05-30 12:19:12 -0300

    Update CozinhaController.java

--- src/main/java/com/food/sendfood/api/controller/CozinhaController.java
+++ src/main/java/com/food/sendfood/api/controller/CozinhaController.java
@@ -3,10 +3,15 @@ package com.food.sendfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.food.sendfood.domain.model.Cozinha;
@@ -30,5 +35,21 @@ public class CozinhaController {
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
     }
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.save(cozinha);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha ) {
+		return cozinhaRepository.findById(id)
+		           .map(record -> {
+		               record.setNome(cozinha.getNome());
+		               Cozinha updated = cozinhaRepository.save(record);
+		               return ResponseEntity.ok().body(updated);
+		           }).orElse(ResponseEntity.notFound().build());
+	}
 
 }
